fd4de4d64f95aa09492c21e02ab322ed6e96b12a
Bruno Ferreira <brunoferreira1999@live.com>
2021-11-11 18:01:41 -0300

    Rota para meus livros

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,6 +10,8 @@ const searchBookByTitleController = require('../controller/searchBookByTitle');
 const searchBookByCategoryController = require('../controller/searchBookByCategory');
 const searchUserByIdController = require('../controller/searchUserById');
 const searchAllBooksController = require('../controller/searchAllBooks');
+const searchBookByUserController = require('../controller/searchBookByUser');
+
 
 routes.post('/login', loginController);
 routes.post('/user-registration', userRegistrationController);
@@ -18,6 +20,7 @@ routes.post('/book-registration', bookRegistrationController);
 routes.post('/searchBookByTitle', searchBookByTitleController);
 routes.post('/searchBookByCategory', searchBookByCategoryController);
 routes.post('/searchUserById', searchUserByIdController);
+routes.post('/searchBookByUser', searchBookByUserController);
 
 routes.get('/searchAllBooks', searchAllBooksController);
 
